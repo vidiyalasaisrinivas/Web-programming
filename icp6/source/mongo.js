a09e0f5bbe3deabff6fd0c985b4c95d810f15af9
vidiyalasaisrinivas <42701975+vidiyalasaisrinivas@users.noreply.github.com>
2018-09-29 19:58:49 -0500

    icp6

---
+++ icp6/source/mongo.js
@@ -0,0 +1,120 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var bodyParser = require("body-parser");
+var express = require('express');
+var cors = require('cors');
+var app = express();
+
+var url='mongodb://sai:srinivas1@ds119343.mlab.com:19343/mlabdb';//1.Modify this url with the credentials of your db name and password.
+var ObjectID = require('mongodb').ObjectID;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/create', function (req, res) {
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+        insertDocument(db, req.body, function() {
+            res.write("Successfully inserted");
+            res.end();
+        });
+    });
+});
+
+app.get('/get', function (req, res) {
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+
+        db.collection('lesson7').find().toArray(function(err, result){
+            if(err)
+            {
+                res.write("get Failed");
+                res.end();
+            }else
+            {
+
+                res.send(JSON.stringify(result));
+            }
+            console.log("Got All Documents");
+
+        });
+    });
+
+});
+
+app.get('/delete/:toBeDeleted_id', function (req, res) {
+    // 2.Connect to MongoDB . Handle the error and write the logic for deleting the desired book
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+        deleteDocument(db, req.body, function() {
+            res.write("Successfully inserted");
+            res.end();
+        });
+    });
+
+
+
+
+});
+
+
+app.get('/update/:toBeUpdated_id', function (req, res) {
+    //3.connect to MongoDB. Handle the error and write the logic for updating the selected field
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var myquery = { _id: ObjectID(req.params.toBeUpdated_id)};
+        var newvalues = {$set: {bookName:req.query.bookName,authorName:req.query.authorName,ISBN:req.query.ISBN } };
+        console.log("myquery"+myquery,"newvalues"+newvalues);
+        db.collection("lesson7").updateOne(myquery, newvalues,function(err, res) {
+            if (err) throw err;
+            console.log(res.result.nModified + " record(s) updated");
+            db.close();
+        });
+    });
+});
+
+//insert
+var insertDocument = function(db, data, callback) {
+    db.collection('lesson7').insertOne( data, function(err, result) {
+        if(err)
+        {
+            res.write("Registration Failed, Error While Registering");
+            res.end();
+        }
+        console.log("Inserted a document into the books collection.");
+        callback();
+    });
+};
+//delete
+var deleteDocument = function(db, data, callback) {
+    db.collection('lesson7').deleteOne( data, function(err, result) {
+        if(err)
+        {
+            res.write("Registration Failed, Error While Registering");
+            res.end();
+        }
+        console.log("Inserted a document into the books collection.");
+        callback();
+    });
+};
+//update
+
+
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port)
+});
